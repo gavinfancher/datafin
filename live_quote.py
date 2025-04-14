a104fadb05ce08df2809581d509a96f39dfc9e0c
gavinfancher <gavinfancher02@gmail.com>
2025-04-14 13:28:08 -0500

    more updates to reference files

--- live_quote.py
+++ live_quote.py
@@ -1,5 +1,6 @@
 from DataFin import FMPClient
 import os
+import sys
 import dotenv
 import time
 
@@ -13,22 +14,24 @@ aws_secret_key = os.getenv('AWS_SECRET_ACCESS_KEY')
 
 fmp = FMPClient(fmp_api_key)
 
+symbol = sys.argv[1]
+print(symbol)
+
 try:
     print("Press Ctrl+C to stop the program")
-    print(f"{'Price':<11} {'Change':<11}")
+    print(f"{'Price':<10} {'Change':<10}")
     print("-" * 10)
     
     while True:
         amzn_day_start = fmp.get_end_of_day_full('amzn')[0]['open']
         
-        amzn = fmp.get_live_price('amzn')
-        price = amzn[0]['price']
-        pct_change = round((amzn[0]['change'] / amzn_day_start) * 100, 2)
+        data = fmp.get_live_price(symbol)
+        price = data[0]['price']
+        pct_change = round((data[0]['change'] / amzn_day_start) * 100, 2)
         
 
-        print(f"{price:<11}' {pct_change:>6}%")
+        print(f"{price:<10} {pct_change:>6}%")
         
-        time.sleep(0.5)
 
 except KeyboardInterrupt:
     print("\nProgram stopped by user")
